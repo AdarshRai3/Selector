f4f2160e5d54476d8ca889b181668dffce34f627
AdarshRai3 <adarshaibusiness@gmail.com>
2025-02-07 11:49:54 +0530

    Intialise : Selector Project

---
+++ src/main/java/com/bootcoding/selector/entity/InterviewQuestion.java
@@ -0,0 +1,4 @@
+package com.bootcoding.selector.entity;
+
+public class InterviewQuestion {
+}
---
+++ src/test/java/com/bootcoding/selector/SelectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcoding.selector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
